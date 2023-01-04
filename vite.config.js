8e2cb68569bd1c8c31ce088f03e1fe581e3cd0f5
Afrad Bhuiyan <afradbhuiyan2021@gmail.com>
2023-01-04 23:52:26 +0600

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const mode = process.env.NODE_ENV;
+import { defineConfig } from "vite";
+
+export default defineConfig({
+
+     base: mode === 'production' ? '/vite-app/': '/',
+     server: {
+          open: './index.html',
+          port: 3000
+     },
+
+     build: {
+          rollupOptions: {
+               input: {
+                    main: path.resolve(__dirname, 'index.html')
+               }
+          }
+     }
+});
