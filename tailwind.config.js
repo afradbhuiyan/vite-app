972045a0dba981da0de403f796c266089ca3ed88
Afrad Bhuiyan <afradbhuiyan2021@gmail.com>
2023-01-06 23:24:22 +0600

    [feature] add a basic header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './*.html',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '1rem'
+    },
+
+    fontFamily: {
+      sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+    },
+
     extend: {},
   },
   plugins: [],
